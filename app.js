5974293ed28eba94160e97c77c318cd637ad7cb7
Massimo Liani <massimo.liani@goooled.com>
2021-08-03 12:59:49 +0200

    second

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! I\'m Massimo and I\'m alive!'));
+app.get("/", (req, res) =>
+  res.send(
+    "Hello World! I'm Massimo and I'm alive! and just change a little bit"
+  )
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
